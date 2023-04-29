cb69d7e152153e017c59aa65e7410547f3ade371
Hridoy Krisna Das <37345409+hridoykrisnadas@users.noreply.github.com>
2023-04-29 14:12:37 +0600

    Update Utils.java

--- app/src/main/java/com/hridoykrisna/email/Utils.java
+++ app/src/main/java/com/hridoykrisna/email/Utils.java
@@ -1,7 +1,7 @@
 package com.hridoykrisna.email;
 
 public class Utils {
-    public static final String EMAIL = "hridoy@productimageedit.com";
+    public static final String EMAIL = "your-email-here";
 
     //This is your from email password
     public static final String PASSWORD = "your-password-here";
